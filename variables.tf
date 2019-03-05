806f4f1b91d1bc41a06921219a6a343bdd4710c8
Richard Gutkowski <richard@ososolution.com>
2019-03-05 11:31:35 +0000

    added vars

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "sns_infrastructure_arn" {}
+
+variable "s3_policy_change_rule_name" {}
+
+variable "cloudtrail_name" {}
+
+variable "cloudwatch_log_group_name" {}
+
+variable "common_tags" {}
