3c1ec1b06518da5ad92e47fdeff9b74a0d77987f
Richard Gutkowski <richard@ososolution.com>
2019-03-05 11:22:36 +0000

    First commit

---
+++ aws_iam.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_policy" "cwl_logs" {
+  name = "${var.cloudwatch_log_group_name}-policy"
+  description = "Policy for adding cloudwatch logs"
+
+  policy = <<EOF
+{
+      "Statement":[
+        {
+          "Effect":"Allow",
+          "Action":[
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource":[
+            "${aws_cloudwatch_log_group.cwl_log_group.arn}"
+          ]
+        }
+}
+EOF
+}
+
+
+resource "aws_iam_role" "cwl_logs" {
+  name = "${var.cloudwatch_log_group_name}-role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "cwl_log_role_attachement" {
+  role = "${aws_iam_role.cwl_logs.name}"
+  policy_arn = "${aws_iam_policy.cwl_logs.arn}"
+}
---
+++ aws_s3_events.tf
@@ -0,0 +1,47 @@
+resource "aws_cloudwatch_event_rule" "s3_policy_change_rule" {
+  name = "${var.s3_policy_change_rule_name}"
+  description = "Captures changes to S3 buckets"
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.s3"
+  ],
+  "detail-type": [
+    "AWS API calls via Cloudtrail"
+  ],
+  "detail": {
+    "eventSource": [
+      "s3.amazonaws.com"
+    ],
+    "eventName": [
+      "DeleteBucket",
+      "CreateBucket",
+      "DeleteBucketCors",
+      "DeleteBucketLifecycle",
+      "DeleteBucketPolicy",
+      "DeleteBucketReplication",
+      "DeleteBucketTagging",
+      "DeleteBucketWebsite",
+      "PutBucketAcl",
+      "PutBucketCors",
+      "PutBucketLifecycle",
+      "PutBucketLogging",
+      "PutBucketNotification",
+      "PutBucketPolicy",
+      "PutBucketReplication",
+      "PutBucketRequestPayment",
+      "PutBucketTagging",
+      "PutBucketVersioning",
+      "PutBucketWebsite",
+    ]
+  }
+}
+PATTERN
+}
+
+resource "aws_cloudwatch_event_target" "sns" {
+  arn = "${var.sns_infrastructure_arn}"
+  rule = "${aws_cloudwatch_event_rule.s3_policy_change_rule.name}"
+  target_id = "SendToSNS"
+}
