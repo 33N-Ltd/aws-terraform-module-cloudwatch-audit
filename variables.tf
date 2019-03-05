b1a96a61bb37792ef815a70ecb406a685bd63380
Richard Gutkowski <richard@ososolution.com>
2019-03-05 13:08:32 +0000

    removed bad vars

--- variables.tf
+++ variables.tf
@@ -2,11 +2,7 @@ variable "sns_topic_name" {}
 
 variable "sns_display_name" {}
 
-variable "sns_infrastructure_arn" {}
-
 variable "s3_policy_change_rule_name" {}
-
-variable "cloudtrail_name" {}
 
 variable "cloudwatch_log_group_name" {}
 
