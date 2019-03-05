fcf607ac34adf30051764149fa7b4265ddb7308b
Richard Gutkowski <richard@ososolution.com>
2019-03-05 13:06:41 +0000

    tag is broken, needs to be fixed

--- aws_cwl_log_group.tf
+++ aws_cwl_log_group.tf
@@ -1,7 +1,5 @@
 resource "aws_cloudwatch_log_group" "cwl_log_group" {
   name = "${var.cloudwatch_log_group_name}"
 
-  tags = "${merge(var.common_tags
-  , map("Name", "${var.cloudwatch_log_group_name")
-  )}"
+//  tags = '${merge(var.common_tags, map("Name", "${var.cloudwatch_log_group_name"))}'
 }
